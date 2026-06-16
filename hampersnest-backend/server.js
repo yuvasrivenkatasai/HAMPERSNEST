c13313c870d43c8545c9fa9b7613e2e617109e94
lavanya\peketi <peketinaveen87@gmail.com>
2026-06-16 21:34:07 +0530

    changes

--- hampersnest-backend/server.js
+++ hampersnest-backend/server.js
@@ -29,6 +29,7 @@ import heroBannerRoutes from './routes/heroBanner.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // Connect to Database
